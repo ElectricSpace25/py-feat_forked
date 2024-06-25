341b17396e1d556a7c622daff9eb4a146d8f6341
Yara Studer <yarastuder@gmail.com>
2024-06-25 15:39:00 -0400

    Modified test.py

--- test.py
+++ test.py
@@ -9,9 +9,10 @@ skip_frames = 30
 batch_size = 1
 
 print("▶ Starting video detection")
-video_prediction = detector.detect_video(video_path, skip_frames=skip_frames, batch_size=batch_size, memory_storage=True)
+video_prediction = detector.detect_video(video_path, skip_frames=skip_frames, batch_size=batch_size, memory_storage=True, 
+                                         detect_poses=False, detect_aus=False, detect_identities=False, detect_emotions=False)
 print("✓ Video detection complete")
 
 print("▶ Saving to CSV")
-video_prediction.to_csv('output.csv')
+video_prediction.to_csv('output3.csv')
 print("✓ CSV saved")
